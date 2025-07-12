75d45f449d0b53991edddb61be8392407c527a1d
Kantore-jg <kantorejeangildas@gmail.com>
2025-07-12 10:33:49 +0200

    Add files via upload

--- Trivia Questions/303 - quizzler-app-start/data.py
+++ Trivia Questions/303 - quizzler-app-start/data.py
@@ -1,102 +1,124 @@
-question_data = [
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The HTML5 standard was published in 2014.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The first computer bug was formed by faulty wires.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Linus Torvalds created Linux and Git.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "AMD created the first consumer 64-bit processor.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "'HTML' stands for Hypertext Markup Language.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    }
-]
+import requests
+
+parameters = {
+    "amount":10,
+    "type":"boolean",
+}
+
+response = requests.get(url="https://opentdb.com/api.php",params=parameters)
+response.raise_for_status()
+
+data = response.json()
+
+question_data = data['results']
+#print(question_data)
+
+
+
+
+
+# question_data ={
+#   "response_code": 0,
+#   "results": [
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "Kraft &quot;Cheez Whiz&quot; contains cheese culture, but doesn&#039;t actually contain cheese",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "SCP-173 was the first SCP article written for the web-based collaborative fiction project known as the &quot;SCP Foundation&quot;",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "The British organisation CAMRA stands for The Campaign for Real Ale.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "Instant mashed potatoes were invented by Canadian Edward Asselbergs in 1962.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "The scientific name for the Southern Lights is Aurora Australis?",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "Francis Bacon died from a fatal case of pneumonia while he was attempting to preserve meat by stuffing a chicken with snow.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "The sum of all the numbers on a roulette wheel is 666.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "Furby was released in 1998.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "Coca-Cola&#039;s original colour was green.",
+#       "correct_answer": "False",
+#       "incorrect_answers": [
+#         "True"
+#       ]
+#     },
+#     {
+#       "type": "boolean",
+#       "difficulty": "medium",
+#       "category": "General Knowledge",
+#       "question": "&quot;Santa Claus&quot; is a variety of melon.",
+#       "correct_answer": "True",
+#       "incorrect_answers": [
+#         "False"
+#       ]
+#     }
+#   ]
+# }
